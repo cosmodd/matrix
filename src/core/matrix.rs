eeb91421524a74c789cd19e9ab1c7c9f4c156cb8
Maxim RATTEZ <lolesqua@gmail.com>
2025-12-23 13:34:45 +0100

    feat: add matrix.rs with some basic implementations

---
+++ src/core/matrix.rs
@@ -0,0 +1,89 @@
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug)]
+pub struct Matrix<K> {
+    shape: (usize, usize),
+    data: Vec<K>,
+}
+
+impl<K> Matrix<K> {
+    pub fn shape(&self) -> (usize, usize) {
+        self.shape
+    }
+
+    pub fn is_square(&self) -> bool {
+        self.shape.0 == self.shape.1
+    }
+}
+
+impl<K> Clone for Matrix<K>
+where
+    K: Clone,
+{
+    fn clone(&self) -> Self {
+        Matrix {
+            shape: self.shape,
+            data: self.data.clone(),
+        }
+    }
+}
+
+impl<K, const W: usize, const H: usize> From<[[K; W]; H]> for Matrix<K>
+where
+    K: Clone,
+{
+    fn from(values: [[K; W]; H]) -> Self {
+        let mut data = Vec::<K>::with_capacity(W * H);
+
+        for x in 0..W {
+            for y in 0..H {
+                data.push(values[y][x].clone());
+            }
+        }
+
+        Matrix {
+            shape: (W, H),
+            data,
+        }
+    }
+}
+
+impl<K> fmt::Display for Matrix<K>
+where
+    K: fmt::Display + fmt::Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let (width, height) = self.shape;
+
+        let mut col_widths = vec![0; width];
+        for col in 0..width {
+            for row in 0..height {
+                let index = col * height + row;
+                let length = self.data[index].to_string().len();
+                col_widths[col] = col_widths[col].max(length);
+            }
+        }
+
+        for row in 0..height {
+            let delim_chars = match row {
+                0 => ('⎡', '⎤'),
+                r if r == height - 1 => ('⎣', '⎦'),
+                _ => ('⎢', '⎢'),
+            };
+
+            write!(f, "{} ", delim_chars.0)?;
+            for col in 0..width {
+                let index = col * height + row;
+                let value = self.data[index].to_string();
+                write!(f, "{:>width$}", value, width = col_widths[col])?;
+                if col < width - 1 {
+                    write!(f, "  ")?;
+                }
+            }
+            writeln!(f, " {}", delim_chars.1)?;
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use crate::core::matrix::Matrix;
+
+mod core;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Matrix::from([
+        [1.1, 2.2, 3.3],
+        [4.4, 5.0, 6.0],
+        [7.0, 8.0, 9.0],
+    ]);
+
+    println!("{:?}", a);
+    println!("{}", a);
 }
