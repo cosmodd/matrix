cc7fea235b55bb33ed49d931e6ecf3757e72f1cb
Maxim RATTEZ <lolesqua@gmail.com>
2026-01-13 14:44:36 +0100

    feat: add lerp function for linear interpolation between values

--- src/core/ops.rs
+++ src/core/ops.rs
@@ -1,6 +1,7 @@
 use crate::core::Vector;
 use crate::traits;
 use crate::traits::Field;
+use std::ops;
 
 pub fn linear_combination<K: Field>(vectors: &[Vector<K>], coeffs: &[K]) -> Vector<K> {
     assert!(!vectors.is_empty(), "vectors array must not be empty");
@@ -20,9 +21,14 @@ pub fn linear_combination<K: Field>(vectors: &[Vector<K>], coeffs: &[K]) -> Vect
     result
 }
 
+pub fn lerp<T>(u: T, v: T, coeff: f32) -> T where T: ops::Mul<f32, Output = T> + ops::Add<Output = T> + ops::Sub<Output = T> + Clone {
+    (v - u.clone()) * coeff + u
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::core::Matrix;
 
     #[test]
     fn test_linear_combination() {
@@ -34,5 +40,27 @@ mod tests {
         let v1 = Vector::from([1., 2., 3.]);
         let v2 = Vector::from([0., 10., -100.]);
         assert_eq!(linear_combination(&[v1, v2], &[10., -2.]), Vector::from([10., 0., 230.]));
+    }
+
+    #[test]
+    fn test_lerp() {
+        assert_eq!(lerp(0., 1., 0.), 0.);
+        assert_eq!(lerp(0., 1., 1.), 1.);
+        assert_eq!(lerp(0., 1., 0.5), 0.5);
+        assert_eq!(lerp(21., 42., 0.3), 27.3);
+        assert_eq!(lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3), Vector::from([2.6, 1.3]));
+
+        let a = Matrix::from_rows([
+            [2., 1.],
+            [3., 4.]
+        ]);
+        let b = Matrix::from_rows([
+            [20., 10.],
+            [30., 40.]
+        ]);
+        assert_eq!(lerp(a.clone(), b.clone(), 0.5), Matrix::from_rows([
+            [11., 5.5],
+            [16.5, 22.]
+        ]));
     }
 }
