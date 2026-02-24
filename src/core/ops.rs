22b6a945134af274e2d75aac6bbf3ceb54ce0f65
Maxim RATTEZ <lolesqua@gmail.com>
2026-02-24 15:02:17 +0100

    feat: add perspective projection function for Matrix struct

--- src/core/ops.rs
+++ src/core/ops.rs
@@ -1,15 +1,33 @@
 #![allow(dead_code)]
 
+use crate::core::Matrix;
+use crate::traits::{Field, Trig};
 use std::ops;
 
 pub fn lerp<T>(u: T, v: T, coeff: f32) -> T where T: ops::Mul<f32, Output = T> + ops::Add<Output = T> + ops::Sub<Output = T> + Clone {
     (v - u.clone()) * coeff + u
 }
 
+pub fn perspective_projection<T>(fov: T, aspect: T, near: T, far: T) -> Matrix<T>
+where
+    T: Field + Trig + From<f64>
+{
+    let mut mat = Matrix::<T>::from_elem(T::zero(), 4, 4);
+    let inverse_tan_fov = T::one() / (fov / 2.0.into()).to_radians().tan();
+    let inverse_aspect = T::one() / aspect;
+
+    mat[(0, 0)] = inverse_aspect * inverse_tan_fov;
+    mat[(1, 1)] = inverse_tan_fov;
+    mat[(2, 2)] = -((far + near) / (far - near));
+    mat[(3, 2)] = -((<f64 as Into<T>>::into(2.0) * far * near) / (far - near));
+    mat[(2, 3)] = -T::one();
+
+    mat
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::core::Matrix;
     use crate::core::Vector;
 
     #[test]
